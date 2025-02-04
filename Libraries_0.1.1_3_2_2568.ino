752c9526a42be149ed47dc421de032db6ae1f9ac
meteamwork-dev <meteamwork.dev@gmail.com>
2025-02-04 15:43:36 +0700

    Add files via upload

---
+++ Libraries_0.1.1_3_2_2568.ino
@@ -0,0 +1,27 @@
+#include "MEBoard.h"
+
+void setup() {
+   
+  pinmode();
+  TimeSensor.setInterval(3000,ReadSensor);  //10000 = 10Sec 3000
+  Serial.begin(19200);  // for debug
+  slave.begin(9600); // baud-rate at 9600  RS485(1)  
+  slave2.begin(9600); // baud-rate at 9600  RS485(2) 
+}
+void ReadSensor(){
+  ReadAI();
+/////////////forAIset///////////////////////
+
+//////////////////////////////////////////
+}
+void loop() {
+   TimeSensor.run();
+   ReadDI();
+   modbusread();
+   Eeprom(); 
+ /////////////programing here//////////////////
+    
+    
+ /////////////////////////////////////////////  
+  Register();
+}
